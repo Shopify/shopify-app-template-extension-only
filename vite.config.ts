3c0809f4b0c73b5f462d14bcc76f8249b1d5b70b
Melissa Luu <melissa.luu@shopify.com>
2026-03-31 17:59:58 -0400

    Update template to match original playground version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,52 @@
 import { defineConfig } from "vite";
-import { preact } from "@preact/preset-vite";
+import preact from "@preact/preset-vite";
+
+if (
+  process.env.HOST &&
+  (!process.env.SHOPIFY_APP_URL ||
+    process.env.SHOPIFY_APP_URL === process.env.HOST)
+) {
+  process.env.SHOPIFY_APP_URL = process.env.HOST;
+  delete process.env.HOST;
+}
+
+const host = new URL(process.env.SHOPIFY_APP_URL || "http://localhost")
+  .hostname;
+let hmrConfig;
+
+if (host === "localhost") {
+  hmrConfig = {
+    protocol: "ws",
+    host: "localhost",
+    port: 64999,
+    clientPort: 64999,
+  };
+} else {
+  hmrConfig = {
+    protocol: "wss",
+    host: host,
+    port: parseInt(process.env.FRONTEND_PORT) || 8002,
+    clientPort: 443,
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: [host],
+    cors: {
+      preflightContinue: true,
+    },
+    port: Number(process.env.PORT || 3000),
+    hmr: hmrConfig,
+    fs: {
+      // See https://vitejs.dev/config/server-options.html#server-fs-allow for more information
+      allow: [".", "../shared", "../dist", "../node_modules"],
+    },
+  },
   root: "home",
   base: "./",
+  envPrefix: "SHOPIFY_",
   plugins: [preact()],
   esbuild: {
     legalComments: "external",
@@ -13,6 +55,7 @@ export default defineConfig({
     target: "es2022",
     modulePreload: false,
     outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       external: ["preact-render-to-string"],
     },
