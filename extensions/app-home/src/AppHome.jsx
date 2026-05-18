a01d08f6eb430c2cd5a200f7b48a17b1b594382e
Elana Kopelevich <elana.kopelevich@shopify.com>
2026-05-18 10:06:39 -0600

    Add s-app-nav

--- extensions/app-home/src/AppHome.jsx
+++ extensions/app-home/src/AppHome.jsx
@@ -3,6 +3,7 @@ import {LocationProvider, ErrorBoundary, Router, Route} from 'preact-iso';
 
 import HomePage from './pages/HomePage.jsx';
 import FaqPage from './pages/FaqPage.jsx';
+import SettingsPage from './pages/SettingsPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 
 export default async () => {
@@ -12,10 +13,14 @@ export default async () => {
 function App() {
   return (
     <LocationProvider>
+      <s-app-nav>
+        <s-link href="/settings">Settings</s-link>
+      </s-app-nav>
       <ErrorBoundary>
         <Router>
           <Route path="/" component={HomePage} />
           <Route path="/faq/:id" component={FaqPage} />
+          <Route path="/settings" component={SettingsPage} />
           <Route default component={NotFoundPage} />
         </Router>
       </ErrorBoundary>
